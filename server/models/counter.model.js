aa81bd03390732c6caae565b66cb3cb6e3692da9
Sanjay S P <59553361+sanjaysp2001@users.noreply.github.com>
2024-07-23 21:09:45 +0530

    Seperating backend and frontend

--- server/models/counter.model.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const CounterSchema = new mongoose.Schema({
-  seq: { type: Number, default: 10000000000 },
-});
-
-var counter = mongoose.model("counter", CounterSchema);
-
-export default counter;
