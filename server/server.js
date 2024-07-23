aacfd770ef422fc96618bdc2d4ea2f9511f702cc
Sanjay S P <59553361+sanjaysp2001@users.noreply.github.com>
2024-07-23 11:30:12 +0530

    Backend Code created

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 import express from "express";
+import { configDotenv } from "dotenv";
+configDotenv();
 
 const app = express();
+import connectToDB from "./database.js";
+const port = process.env.PORT || 8000;
+import urlController from "./controllers/url.controller.js";
 
-app.get("/",(req,res)=>{
-    res.end("This server is running!")
-})
-app.listen(8000, () => {
-  console.log("Server is running on http://localhost:8000");
-});
+// For parsing application/json
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/url", urlController);
+
+connectToDB
+  .then((connection) => {
+    console.log("Database connection successful");
+    app
+      .listen(port, () => {
+        console.log(`Server is running on ${port}`);
+      })
+      .on("error", (err) => {
+        console.log("Server creation failed:\n", err);
+      });
+  })
+  .catch((err) => console.log("Error when connecting to Database:\n", err));
